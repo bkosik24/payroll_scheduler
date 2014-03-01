d8dec26e41f5f27b22494c6833cddbaea8adf513
Brian Kosik <bkosik24@gmail.com>
2014-03-01 09:42:11 -0600

    clean up tests

--- spec/date_parser_spec.rb
+++ spec/date_parser_spec.rb
@@ -14,19 +14,18 @@ describe DateParser do
     end
 
     it "should return the value of an array" do
-      dates = DateParser.grab_payday_for_year(@year, @valid_date)
+      dates = DateParser.grab_payday_for_year(@year, @valid_date, nil)
       dates.is_a?(Array)
     end
   end
 
   context "#valid_weekday" do
+    let(:date){ DateParser.valid_weekday(@year, 2, @valid_date, nil) }
     it "should return a date object" do
-      date = DateParser.valid_weekday(@year, 2, @valid_date)
       date.is_a?(Date)
     end
 
     it "should return the date passed in if valid date" do
-      date = DateParser.valid_weekday(@year, 2, @valid_date)
       date.should == Date.new(@year, 2, @valid_date)
     end
   end
